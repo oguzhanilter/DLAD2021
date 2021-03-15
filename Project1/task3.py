624d4a5862c3203ab0026818bef9abebb53699b9
oguzhan ilter <33988750+oguzhanilter@users.noreply.github.com>
2021-03-16 02:49:56 +0300

    discontinuity happens

--- Project1/task3.py
+++ Project1/task3.py
@@ -30,40 +30,67 @@ def visualize(image, xy, color_map):
         color_map:  dictionary
                     maps numeric semantic labels to a BGR color for visualization   
     """
+    """
     angles = xy[3,:]*57.295779513
-    bins = int((max(angles)-min(angles))/0.18)
-    a = angles[:-1] 
-    b = angles[1:]
-    diff = abs(angles[-1] - angles[1:])
-   
-    _ , bin_edges = np.histogram(angles, bins=bins)
-    xy= xy[0:3,:].astype(int)
+    inside_fov = np.logical_and((angles > -40), (angles < 40))
+    inside_frame_indices = np.argwhere(inside_fov).flatten()
+    xy = xy[:, inside_frame_indices]
+    """
+    angles_vert = xy[4,:]*57.295779513
+    a = 0
+    min_l = -24.9 - a 
+    max_l = 2 + a
+
+
+    d = np.logical_and( ( angles_vert>=min_l ) , ( angles_vert<=max_l ) )
+    d_i  = np.argwhere(d).flatten()
+    xy = xy[:,d_i]
+    angles_vert = angles_vert[d_i]
+ 
+
+    _ , bin_edges = np.histogram(angles_vert, bins=64) # ,range=(min_l,max_l)
+
+    xy = np.around(xy[0:3,:])
+    xy = xy.astype(int)
     x       = xy[0,:]
     y       = xy[1,:]
 
-    for i in range(bins):
-        if i != bins:
-            group       = np.logical_and( ( angles>=bin_edges[i] ) , ( angles<=bin_edges[i+1] ) )
-        else:
-            group       = np.logical_and( ( angles>=bin_edges[i] ) , ( angles<bin_edges[i+1] ) )
+    
 
+    for i in range(64):
+        if i != 64:
+            group       = np.logical_and( ( angles_vert>=bin_edges[i] ) , ( angles_vert<bin_edges[i+1] ) )
+        else:
+            group       = np.logical_and( ( angles_vert>=bin_edges[i] ) , ( angles_vert<=bin_edges[i+1] ) )
+
+        color = color_map[i%4]
         group_inds  = np.argwhere(group).flatten()
 
-        sorted_inds = x[group_inds].argsort()
-        fliped_inds = np.flip(sorted_inds)
+        for j in range(len(group_inds)):
+            image = cv2.circle(image, (xy[1,group_inds[j]], xy[0,group_inds[j]]), radius=1, color=color, thickness=-1)
+        
+        #im = Image.fromarray(image)
+        #im.show()
 
-        inds = group_inds[fliped_inds]
-              
+        # For debugginh to see every different angle bin 
+        """
 
-        xy[2, inds] = i%4
-
+        print(i)
+        im = np.asarray(image).astype(np.uint8)
+        
+        cv2.imshow('asd', im)
+        cv2.waitKey(0)  
+  
+        #xy[2,group_inds] = i % 4
+        """
+        
+    """
     for i in range(len(xy[0])):
         
-    
-        color = color_map[xy[2, i]]
+        color = color_map[int(xy[2, i])]
         image = cv2.circle(image, (xy[1,i], xy[0,i]), radius=1, color=color, thickness=-1)
         #image[xy[0,i], xy[1,i],:] = [color[2], color[1],color[0]]
-
+    """
     return np.asarray(image).astype(np.uint8)
 
 
@@ -92,12 +119,14 @@ def filter_indices_xy(xy, image_size):
     y = xy[0,:]
     indices = xy[2,:]
     angles = xy[3,:]
+    angles_vert = xy[4,:]
 
-    inside_frame_x = np.logical_and((x > 0), (x < image_size[0]))
-    inside_frame_y = np.logical_and((y > 0), (y < image_size[1]))
-    inside_frame_indices = np.argwhere(np.logical_and(inside_frame_x,inside_frame_y)).flatten()
+    #inside_frame_x = np.logical_and((x > 0), (x < image_size[0]))
+    #inside_frame_y = np.logical_and((y > 0), (y < image_size[1]))
+    #inside_frame_indices = np.argwhere(np.logical_and(inside_frame_x,inside_frame_y)).flatten()
 
-    filtered_xy = np.stack((x[inside_frame_indices],y[inside_frame_indices],indices[inside_frame_indices],angles[inside_frame_indices] ))
+    # filtered_xy = np.stack((x[inside_frame_indices],y[inside_frame_indices],indices[inside_frame_indices],angles[inside_frame_indices],angles_vert[inside_frame_indices] ))
+    filtered_xy = np.stack((x,y,indices,angles,angles_vert ))
     
     return filtered_xy 
 
@@ -130,7 +159,10 @@ def projection_3D_2D(points, extrinsic, intrinsic):
     front_hemisphere_x = points[front_hemisphere_indices, 0]
     front_hemisphere_y = points[front_hemisphere_indices, 1]
     front_hemisphere_z = points[front_hemisphere_indices, 2]
-    front_hemisphere_angles = np.arctan2(front_hemisphere_y,front_hemisphere_x)
+    angles_vertical = np.arctan2(front_hemisphere_y,front_hemisphere_x)
+
+    a = np.sqrt(np.square(front_hemisphere_x )+ np.square(front_hemisphere_y))
+    angles_horizontal = np.arctan2(front_hemisphere_z,a)
     homo_coor = np.ones(len(front_hemisphere_x))
 
     XYZ = np.stack((front_hemisphere_x,front_hemisphere_y,front_hemisphere_z,homo_coor))
@@ -141,8 +173,9 @@ def projection_3D_2D(points, extrinsic, intrinsic):
     xy = xy / xy[2,None]
 
     xy[2,:] = front_hemisphere_indices
-    xy = np.vstack((xy, front_hemisphere_angles))  
-
+    xy = np.vstack((xy, angles_vertical)) 
+    xy = np.vstack((xy, angles_horizontal)) 
+    a=9
     return xy
 
 if __name__ =="__main__":
@@ -155,7 +188,7 @@ if __name__ =="__main__":
     cam_mat_P           = data['P_rect_20']     # intrinsic camera parameters
     cam_image           = data['image_2']
 
-    label_color_map = [[255,0,0], [0,255,0], [0,0,255,], [255,0,255]]
+    label_color_map = [[255,0,0], [0,255,0], [0,0,255,], [0,255,255]]
 
     xy          = projection_3D_2D(velo_point_cloud,velo_mat_T,cam_mat_P)
     filtered_xy = filter_indices_xy(xy, cam_image.shape)
