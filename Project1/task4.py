corrected lidar rotation speed

Project1/task4.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import data_utils
 from scipy.spatial.transform import Rotation as Rot
+from scipy.interpolate import interp1d
 import numpy as np
 import math
 from PIL import Image
@@ -157,14 +158,14 @@ def corrected_projection_3D_2D(points, extrinsic, intrinsic,lidar_ts,lidar_te,li
     XYZ = np.stack((front_hemisphere_x,front_hemisphere_y,front_hemisphere_z,homo_coor))
         
     projection_matrix = np.matmul(intrinsic,extrinsic) 
-        
+    
     
     #have to process each point separately and correct for distortions
 
     for i in range(len(XYZ[0,:])):
+
+        #read horizontal angle (yaw) of point
         h_angle = angles_horizontal[i]
-        #XYZ[1,i] += a
-        #print(h_angle-lidar_end_yaw)
         
         #find interpolation coefficient of current angle between starting and stopping angle of lidar scan (360°)
         interp = (h_angle - lidar_end_yaw)/(lidar_start_yaw - lidar_end_yaw)
@@ -173,7 +174,8 @@ def corrected_projection_3D_2D(points, extrinsic, intrinsic,lidar_ts,lidar_te,li
         dt =   interp*lidar_ts + (1-interp)*lidar_te - lidar_t
         
         R = Rot.from_euler('z',imu_rot[2]*dt, degrees=False).as_matrix() #Rs*coeff + Re*(1-coeff)
-        T = imu_vel*dt #Ts*interp + Te*(1-interp)
+        T = imu_vel*dt 
+
         RT = create_homo_trans(R,T)
         XYZ[:,i] = np.matmul(RT,XYZ[:,i])
         
@@ -267,19 +269,13 @@ def visualize_task4(cam_image, xy,velo_point_cloud):
     xy = xy.astype(int)
     for i in range(len(xy[0])):
         
-        #if  xy[5,i] > 50:
-        #    continue
-        
         #original index 
         idx = xy[2,i].astype(np.uint32)
-        #if idx > 10:
-        #    continue
-        #velo_point_cloud[idx]
         
         color = hsv2rgb(hue[i],1,1)
         #color = hsv2rgb(velo_point_cloud[xy[2,i].astype(np.uint32),0],1,1)
         
-        cam_image = cv2.circle(cam_image, (xy[1,i], xy[0,i]), radius=1, color=color, thickness=-1)
+        cam_image = cv2.circle(cam_image, (xy[1,i], xy[0,i]), radius=2, color=color, thickness=-1)
         #image[xy[0,i], xy[1,i],:] = [color[2], color[1],color[0]]
         """  
         if idx%10 == 0:
@@ -336,85 +332,113 @@ def hsv2rgb(h, s, v):
     r, g, b = int(r * 255), int(g * 255), int(b * 255)
     return r, g, b
 
+
+def get_frame_name(frame):
+    """Returns frame name as string"""
+    return str(frame).zfill(10)
+
+
+def get_lidar_orientations(lidar_ts,lidar_te,lidar_t):
+    """Calculate start and end orietation (yaw) of lidar
+    
+    Args:
+        lidar_ts (float): starting time of lidar scan
+        lidar_te (float): ending time of lidar scan
+        lidar_t (float): time at which lidar is scanning in positive x direction (forward)
+
+    Return
+        lidar_start_yaw (float): starting orientation of lidar 
+        lidar_end_yaw (float): ending orientation of lidar 
+    """
+    
+    #at lidar_t the lidar scanning  direction is forward
+    #yaw angle is in counter-clockwise direction (angle 0 is in postive x direction)
+    #angle on z axis of lidar scanning direction
+    #remember lidar scans in counter clockwise direction
+    lidar_period = lidar_te-lidar_ts #period that lidar used to complete full rotation
+    lidar_rot = math.pi*2/lidar_period
+    lidar_start_yaw = lidar_rot*(lidar_t-lidar_ts)
+    lidar_end_yaw = lidar_start_yaw - math.pi*2 #giust start + 360° in clockwise direction
+
+    return lidar_start_yaw, lidar_end_yaw
+
+
+
 if __name__ =="__main__":
     print("**** Running task4 ****")
 
-    # load metadata
+
+    REMOVE_DISTORTION = True
+
+    # LOAD TRANFORMATION MATRICES
     R_velo2cam,T_velo2cam = calib_velo2cam(RELATIVE_PATH_TO_DATA + '/calib_velo_to_cam.txt')
     R_imu2velo,T_imu2velo = calib_velo2cam(RELATIVE_PATH_TO_DATA + '/calib_imu_to_velo.txt')
     P_cam2cam = calib_cam2cam(RELATIVE_PATH_TO_DATA + '/calib_cam_to_cam.txt','02')
 
+
     # LOAD DATA FOR specific frame
 
+    #interesting debugging frame = 2
     frame = 37
-    frame_name = str(frame).zfill(10)
+    frame_interp = [-1,0,1] #list of frame offsets used for imu data interpolation -1 = previus, 0 = current, 1=next etc
 
-    #image = Image.open(IMAGE_FILE_PATH + '/'+frame_name+ '.png')
-    image = cv2.imread(IMAGE_FILE_PATH + '/'+frame_name+ '.png')
+    frame_name = get_frame_name(frame)
 
-    imu_vel = load_oxts_velocity(OXTS_FILE_PATH+'/'+frame_name+'.txt')
-    imu_rot = load_oxts_angular_rate(OXTS_FILE_PATH+'/'+frame_name+'.txt')
 
-    velo_point_cloud = load_from_bin(script_path+LIDAR_PATH+'/data/' +frame_name+'.bin')
+    #READ TIMESTAMPS
 
     #ts are start times, te are end times
     lidar_ts = compute_timestamps(LIDAR_PATH + TIMESTAMP_START, frame)
     lidar_te = compute_timestamps(LIDAR_PATH + TIMESTAMP_END, frame)
-    lidar_t = compute_timestamps(LIDAR_PATH + TIMESTAMP, frame)
+    lidar_t = compute_timestamps(LIDAR_PATH + TIMESTAMP, frame) #lidar looking forward
 
     imu_t = compute_timestamps(OXTS_PATH + TIMESTAMP, frame)
+    imu_t_interp = [compute_timestamps(OXTS_PATH + TIMESTAMP, frame+i) for i in frame_interp] #list of prev, current and next timestamps
     camera_t = compute_timestamps(IMAGE_PATH + TIMESTAMP, frame)
 
 
+    #READ IMAGE
+    image = cv2.imread(IMAGE_FILE_PATH + '/'+frame_name+ '.png')
 
+    #READ IMU DATA
+
+    #data points for imu vel and angular vel to do interpolation (for previous, current and next frame)
+    imu_vel_interp = [load_oxts_velocity(OXTS_FILE_PATH+'/'+get_frame_name(frame+i)+'.txt') for i in frame_interp]
+    imu_rot_interp = [load_oxts_angular_rate(OXTS_FILE_PATH+'/'+get_frame_name(frame+i)+'.txt') for i in frame_interp]
     # imu in velo frame
-    imu_vel = np.matmul(imu_vel,R_imu2velo)
-    imu_vel += T_imu2velo[0,:]
-    imu_rot = np.matmul(imu_vel,R_imu2velo)
+    imu_vel_interp = [np.matmul(vel,R_imu2velo) for vel in imu_vel_interp]
+    imu_vel_interp = [vel + T_imu2velo[0,:] for vel in imu_vel_interp]
+    imu_rot_interp = [np.matmul(rot,R_imu2velo) for rot in imu_rot_interp]
+    #create an interpolation function for imu vel and rot
+    imu_vel_f = interp1d(imu_t_interp,imu_vel_interp,axis=0)
+    imu_rot_f = interp1d(imu_t_interp,imu_rot_interp,axis=0)
+    #imu_vel_f(t) = imu data interpolated at timestamp t
+
+    #imu data at timestep lidar is looking forward (use this as "average" imu data during correction)
+    imu_vel = imu_vel_f(lidar_t)
+    imu_rot = imu_rot_f(lidar_t)
 
 
-    # create distortion R and T for both start and end timestamps of lidar
-    # Rs and Ts are distortion corrected R and T (in velo frame of reference) for timestamp ts
-    # Re and Te for timestamp te
-
-    #at lidar_t the lidar scanning  direction is forward
-    #yaw angle is in counter-clockwise direction (angle 0 is in postive x direction)
-
-    dts = lidar_t-lidar_ts #seconds between start of scan and lidar pointing forward
-    dte = lidar_te-lidar_te #seconds between forward and end of scan
-
-    #CURRENTLY NOT USED
-    Rs  = Rot.from_euler('z',imu_rot[2]*dts, degrees=False).as_matrix()
-    Re  = Rot.from_euler('z',-imu_rot[2]*dte, degrees=False).as_matrix()
-
-    #CURRENTLY NOT USED
-    Ts = imu_vel*dts
-    Te = -imu_vel*dte
+    #READ POINT CLOUD
+    velo_point_cloud = load_from_bin(script_path+LIDAR_PATH+'/data/' +frame_name+'.bin')
 
 
-    #angle on z axis of lidar scanning direction
-    #remember lidar scans in counter clockwise direction
-    lidar_rot = math.pi*2*10
-    lidar_start_yaw = lidar_rot*(lidar_t-lidar_ts)
-    lidar_end_yaw = lidar_start_yaw - math.pi*2
+    #get starting and ending lidar orientation 
+    lidar_start_yaw, lidar_end_yaw = get_lidar_orientations(lidar_ts,lidar_te,lidar_t)
 
 
-
-    #RT_velo2cam = np.zeros((4, 4))
-    #RT_velo2cam[:3,:3] = R_velo2cam
-    #RT_velo2cam[:3,3:4] = T_velo2cam
-    #RT_velo2cam[3,3] = 1
+    #create tranformation velo to cam in homogeneous coorindates
     RT_velo2cam = create_homo_trans(R_velo2cam,T_velo2cam)
 
 
-
-
-    #do the main stuff... sorry for the shitty documentation
+    #PROJECT lidar point cloud data into image plane
 
     #switch between corrected and original
-    xy          = corrected_projection_3D_2D(velo_point_cloud,RT_velo2cam,P_cam2cam,
-                                            lidar_ts,lidar_te,lidar_t,imu_rot,imu_vel)
-    #xy          = projection_3D_2D(velo_point_cloud,RT_velo2cam,P_cam2cam)
+    if REMOVE_DISTORTION:
+        xy          = corrected_projection_3D_2D(velo_point_cloud,RT_velo2cam,P_cam2cam,
+                                                lidar_ts,lidar_te,lidar_t,imu_rot,imu_vel)
+    else:
+        xy          = projection_3D_2D(velo_point_cloud,RT_velo2cam,P_cam2cam)
     
     
     filtered_xy  = filter_indices_xy(xy, image.shape)